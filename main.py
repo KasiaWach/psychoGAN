6c086629712527ef3be101a6b8a70b713c12f941
AdamSobieszek <sobiesz.adam@gmail.com>
2021-04-14 14:31:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 sys.path.insert(0, os.getcwd()+"/stylegan2")
 from generator import generator
 def main():
-    main_generator = generator(network_pkl_path="/stylegan2/stylegan2-ffhq-config-f.pkl",
+    main_generator = generator(network_pkl_path="gdrive:networks/stylegan2-ffhq-config-f.pkl",
                                direction_path="stylegan2/stylegan2directions/dominance.npy", coefficient=1.0,
                                truncation=0.7, n_levels=3, n_photos=10, type_of_preview="manipulation",
                                result_dir="/results")
