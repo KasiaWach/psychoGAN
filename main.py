c8f373c4558af9b2c11c24d3c0d14ff85993e8ed
AdamSobieszek <sobiesz.adam@gmail.com>
2021-04-14 15:39:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import matplotlib.pyplot as plt
 from generator import generator
 def main():
     main_generator = generator(network_pkl_path="gdrive:networks/stylegan2-ffhq-config-f.pkl",
-                               direction_path="stylegan2/stylegan2directions/dominance.npy", coefficient=1.0,
-                               truncation=0.7, n_levels=3, n_photos=10, type_of_preview="manipulation",
+                               direction_path="stylegan2/stylegan2directions/dominance.npy", coefficient=1.5,
+                               truncation=0.6, n_levels=3, n_photos=10, type_of_preview="manipulation",
                                result_dir="/results")
     main_generator.change_face()
     main_generator._generator__kwargs()
