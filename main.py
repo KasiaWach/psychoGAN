d1b18fc013d70e3d2e1f0d2c09b7efe1e3ebe369
AdamSobieszek <sobiesz.adam@gmail.com>
2021-04-14 14:16:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from generator import generator
+from stylegan2.generator import generator
 def main():
-    import os
-    print(os.getcwd())
     main_generator = generator(network_pkl_path="/stylegan2/stylegan2-ffhq-config-f.pkl",
                                direction_path="stylegan2/stylegan2directions/dominance.npy", coefficient=1.0,
                                truncation=0.7, n_levels=3, n_photos=10, type_of_preview="manipulation",
