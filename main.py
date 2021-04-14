999f119f236d0901d36b28d045b962768db09ed7
AdamSobieszek <sobiesz.adam@gmail.com>
2021-04-14 14:13:27 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from generator import generator
+def main():
+    import os
+    print(os.getcwd())
+    main_generator = generator(network_pkl_path="/stylegan2/stylegan2-ffhq-config-f.pkl",
+                               direction_path="stylegan2/stylegan2directions/dominance.npy", coefficient=1.0,
+                               truncation=0.7, n_levels=3, n_photos=10, type_of_preview="manipulation",
+                               result_dir="/results")
+    plt.imshow(generator.__generate_preview_face_manip())
+
+
+main()
