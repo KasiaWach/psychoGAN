6427bb738a6307612a4a2b1d122837be080d1855
AdamSobieszek <sobiesz.adam@gmail.com>
2021-05-13 13:02:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ import imageio
 import matplotlib.pyplot as plt
 from generator import generator
 def main():
-    main_generator = generator(network_pkl_path="gdrive:networks/stylegan2-ffhq-config-f.pkl",
-                               direction_name="Dominance", coefficient=1.5,
-                               truncation=0.6, n_levels=3, n_photos=10, type_of_preview="manipulation",
-                               result_dir="results", generator_number=1)
+    # main_generator = generator(network_pkl="gdrive:networks/stylegan2-ffhq-config-f.pkl",
+    #                            direction_name="Dominance", coefficient=1.5,
+    #                            truncation=0.6, n_levels=3, n_photos=10, type_of_preview="manipulation",
+    #                            result_dir="results", generator_number=1)
     # main_generator.change_face()
     # Image.fromarray(main_generator._generator__generate_preview_face_manip(), "RGB").save('wynik.jpg', format='JPEG', subsampling=0, quality=50)
     # main_generator.generate()
+    pass
 
 
 main()
